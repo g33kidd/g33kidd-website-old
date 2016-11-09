21097b23e881aaf0e1b3a9ec60c3ce717d9f0216
Joshua Kidd <kidd.josh.343@gmail.com>
2016-11-08 23:06:02 -0600

    Create a post

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -17,6 +17,29 @@ class PostsController extends Controller
 
 	public function store(Request $request)
 	{
+		// validate the request
+		$this->validate($request, [
+			'title' => 'required|max:255',
+			'body' => 'required',
+			'description' => 'required|max:120',
+			'published' => 'required'
+		]);
+
+		// create the post on the current user
+		$post = $request->user()->posts()->create([
+			'title' => $request->input('title'),
+			'description' => $request->input('description'),
+			'body' => $request->input('body'),
+			'published' => $request->input('published'),
+		]);
+
+		// respond with the newly created post
+		return response()->json($post);
+	}
+
+	public function delete(Post $post)
+	{
+		$post->delete();
 		return response(null, 200);
 	}
 
